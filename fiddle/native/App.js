6f7920b9a4c9d19cdd22c83d60b68e5e4556b472
cauldyclark15 <jcing.jc@gmail.com>
2017-12-28 00:45:11 +0800

    Fix

--- fiddle/native/App.js
+++ fiddle/native/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from '../../packages/fiddle-universal-components/components/button';
 
 export default class App extends React.Component {
   render() {
